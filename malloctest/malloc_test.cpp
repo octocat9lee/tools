0daf9bfcafdd7fb901b36759a055d35d1027e2a0
octocat9lee <295861542@qq.com>
2017-04-26 14:31:58 +0800

    update malloctest test case

--- malloctest/malloc_test.cpp
+++ malloctest/malloc_test.cpp
@@ -1,75 +1,98 @@
-#include <map>
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 #include <iostream>
+#include <map>
+#include <vector>
 
 using namespace std;
 
-const int BLOCK_SIZE = 1024;
+typedef void (*MallocFunc)(const int, const int);
 
-static char *maxAddrPtr = NULL;
-
-void MallocMemory(int size)
+void MallocMemoryInMap(const int size, const int blockSize)
 {
     std::map<int, char*> memoryMap;
-
-    char *p = NULL;
-    for(int i = 0; i < size; ++i)
+    int blockSizeKb = blockSize * 1024;
+    for(int i = 0; i < size / blockSize; ++i)
     {
-        p = new char[BLOCK_SIZE];
+        char *p = new char[blockSizeKb];
         char v = rand() % 128;
-        memset(p, v, BLOCK_SIZE);
+        memset(p, v, blockSizeKb);
         memoryMap.insert(make_pair(i, p));
     }
-
-    if(maxAddrPtr == NULL)
-    {
-        maxAddrPtr = p;
-    }
-    
-    int maxAddrV = rand() % 128;
-    memset(maxAddrPtr, maxAddrV, BLOCK_SIZE);
-    cout << "Set Max Address Value " << maxAddrV << endl;
-    cout << (unsigned long long)maxAddrPtr << " maxAddrPtr[0] " << (int)(maxAddrPtr[0]) << " maxAddrPtr[end] "
-        << (int)(maxAddrPtr[BLOCK_SIZE - 1]) << endl;
-    
-    sleep(2);
+    sleep(5);
     std::map<int, char*>::iterator iter = memoryMap.begin();
     for(; iter != memoryMap.end(); ++iter)
     {
-        if(iter->second == maxAddrPtr)
-        {
-            cout << "delete maxAddrPtr " << (unsigned long long)(iter->second) << endl;
-        }
         delete [] iter->second;
     }
     memoryMap.clear();
-    
-    cout << "Memory Free" << endl;
+    cout << "Free Memory" << endl;
+}
+
+void MallocMemoryInVec(const int size, const int blockSize)
+{
+    std::vector<char*> memoryVec;
+    memoryVec.reserve(size / blockSize);
+    int blockSizeKb = blockSize * 1024;
+    for(int i = 0; i < size / blockSize; ++i)
+    {
+        char *p = new char[blockSizeKb];
+        char v = rand() % 128;
+        memset(p, v, blockSizeKb);
+        memoryVec.push_back(p);
+    }
+    sleep(5);
+    std::vector<char*>::iterator iter = memoryVec.begin();
+    for(; iter != memoryVec.end(); ++iter)
+    {
+        delete [] (*iter);
+    }
+    memoryVec.clear();
+    cout << "Free Memory" << endl;
+}
+
+void MallocMemoryInArray(const int size, const int blockSize)
+{
+    char** memoryArray = new char*[size / blockSize];
+    int blockSizeKb = blockSize * 1024;
+    for(int i = 0; i < size / blockSize; ++i)
+    {
+        char *p = new char[blockSizeKb];
+        char v = rand() % 128;
+        memset(p, v, blockSizeKb);
+        memoryArray[i] = p;
+    }
+    sleep(5);
+    for(int i = 0; i < size / blockSize; ++i)
+    {
+        delete [] memoryArray[i];
+    }
+    delete [] memoryArray;
+    cout << "Free Memory" << endl;
 }
 
 int main()
 {
+    int containerType = 0;
+    int initSize = 0;
+    int randSize = 0;
+    int onceSize = 0;
+    cout << "Please Input Container Type (0:Map 1:Vector 2:Array) "
+        << "Initialize Size[MB], Rand Size[MB] and Once Size [KB]" << endl;
+    cin >> containerType >> initSize >> randSize >> onceSize;
+    initSize *= 1024;
     srand((unsigned)time(NULL));
-    int size = 1024 * 1000;
+
+    MallocFunc mallocFuncArray[3] = {MallocMemoryInMap,
+        MallocMemoryInVec, MallocMemoryInArray};
+
     while(true)
     {
-        cout << "Malloc Size " << size / 1024 << " MB" << endl; 
-        MallocMemory(size);
-        size = 1024 * (1 + (rand() % 200));
+        cout << "Malloc Size " << initSize / 1024 << " MB" << endl;
+        mallocFuncArray[containerType](initSize, onceSize);
+        initSize = 1024 * (1 + (rand() % randSize));
         sleep(10);
     }
     return 0;
 }
-
-//int main()
-//{
-//    for(int i = 1; i < 400; ++i)
-//    {
-//        int size = i * 1024;
-//        cout << "Malloc Size " << size / 1024 << " MB" << endl;
-//        MallocMemory(size);
-//        sleep(2);
-//    }
-//}
